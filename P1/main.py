35afc52934733a660841a104ba1b9e327a2f0506
Sang Shin <shinsa@onid.orst.edu>
2011-11-30 10:24:36 -0800

    Remade directory

---
+++ P1/main.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+MAX_NUM=1000
+MIN_NUM=1
+MULT_NUM_3=3
+MULT_NUM_5=5
+
+def main():
+    answer = 0
+    print "Project Euler ID #1"
+    print "Add all the natural numbers below one thousand that are multiples of 3 or 5"
+    print
+    for num in range(MIN_NUM, MAX_NUM):
+        if (num%3 == 0) | (num%5 == 0):
+            answer += num
+    
+    print "Final Answer: ", answer
+main()
+
