d8849a29d53fa90d4819bb9eda264122ef200619
Sang Shin <shinsa@onid.orst.edu>
2011-12-01 03:40:09 -0800

    Modified main.py

--- P1/main.py
+++ P1/main.py
@@ -2,16 +2,14 @@
 
 MAX_NUM=1000
 MIN_NUM=1
-MULT_NUM_3=3
-MULT_NUM_5=5
 
 def main():
     answer = 0
-    print "Project Euler ID #1"
+    print "Project Euler Problem #1"
     print "Add all the natural numbers below one thousand that are multiples of 3 or 5"
     print
     for num in range(MIN_NUM, MAX_NUM):
-        if (num%3 == 0) | (num%5 == 0):
+        if (num % 3 == 0) | (num % 5 == 0):
             answer += num
     
     print "Final Answer: ", answer
