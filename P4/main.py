6ab2cd5ca6d158c31bcecb7b5d9c56e75ebb6f55
Sang Shin <shinsa@onid.orst.edu>
2011-12-01 03:35:47 -0800

    Finished main.py

--- P4/main.py
+++ P4/main.py
@@ -1,9 +1,15 @@
 #!/bin/env python
 
-#MAX_NUM = 999
-#MIN_NUM = 100
-MAX_NUM = 99
-MIN_NUM = 10
+MAX_NUM = 999
+MIN_NUM = 100
+
+def isPalindrome(n):
+    n_str = str(n)
+    if n_str == n_str[::-1]:
+        return True
+    else:
+        return False
+
 def main():
     largest_pal_num = 0
     answer = 0
@@ -13,7 +19,18 @@ def main():
     print
     print "Find the largest palindrome made from the product of two 3 digit numbers."
     print
-    for num in range(MIN_NUM, MAX_NUM):
-        answer = MIN_NUM * num
-        print MIN_NUM, "*", num, "=", answer
+    for first_num in range(MAX_NUM, MIN_NUM, -1):
+        for second_num in range(MAX_NUM, MIN_NUM, -1):
+            answer = first_num * second_num
+            if isPalindrome(answer) == True:
+                if largest_pal_num < answer:
+                    largest_pal_num = answer
+                else:
+                    continue
+            else:
+                continue
+
+    print "Final Answer: ", largest_pal_num
+
 main()
+
