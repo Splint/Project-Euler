b8df3c9c7163536ee98dcbef9c3329332b90d3c6
Sang Shin <shinsa@onid.orst.edu>
2011-12-09 19:18:38 -0800

    Finished main.py for P5

--- P5/main.py
+++ P5/main.py
@@ -4,7 +4,6 @@ MIN_NUM = 1
 MAX_NUM = 20
 
 def main():
-    smallest_num = 0
     print "Project Euler Problem #5"
     print "2520 is the smallest number that can be divided by each of the numbers from"
     print "1 to 10 without any remainder."
@@ -13,17 +12,15 @@ def main():
     print "numbers from 1 to 20?"
     print
     
-    counter = 0
-    while(smallest_num == 0):
-        counter += 1
-        print counter
-        for num in range(MIN_NUM, (MAX_NUM+1)):
-            if counter % num == 0:
-                if num == MAX_NUM:
-                    smallest_num = counter
+    i = 1
+    for k in range(MIN_NUM, (MAX_NUM + 1)):
+        if i % k > 0:
+            for j in range(MIN_NUM, (MAX_NUM + 1)):
+                if (i * j) % k == 0:
+                    i *= j
                     break
-            else:
-                break
 
-    print "Final Answer: ", smallest_num
-main()
+    print "Final Answer: ", i
+    
+if __name__ == "__main__":
+    main()
