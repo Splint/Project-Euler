b40685442e7cc71bfc7c3f1cb0b16d423a903bda
Sang Shin <shinsa@onid.orst.edu>
2011-12-20 01:54:44 -0800

    Finished P12

--- P12/main.py
+++ P12/main.py
@@ -1,7 +1,25 @@
 #!/bin/env python
+import math
 
+def factors(n):
+    factors = 0
+    for i in xrange(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            factors += 2
+    return factors
+
+def get_answer():
+    a = 0
+    tri = 0
+    while True:
+        a += 1
+        tri += a
+        if factors(tri) > 500:
+            return tri
+            False
 def main():
-    print "Project Euler Problem 12"`
+    print "Project Euler Problem #12"
+    print "Final Answer: ", get_answer()
 
 if __name__ == "__main__":
     main()
