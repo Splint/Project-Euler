194945b744dc927a75649c8d74837b3c17e34adc
Sang Shin <shinsa@onid.orst.edu>
2011-12-20 02:06:43 -0800

    Finished main.py for P5

--- P5/main.py
+++ P5/main.py
@@ -5,13 +5,6 @@ MAX_NUM = 20
 
 def main():
     print "Project Euler Problem #5"
-    print "2520 is the smallest number that can be divided by each of the numbers from"
-    print "1 to 10 without any remainder."
-    print
-    print "What is the smallest positive number that is evenly divisible by all the"
-    print "numbers from 1 to 20?"
-    print
-    
     i = 1
     for k in range(MIN_NUM, (MAX_NUM + 1)):
         if i % k > 0:
