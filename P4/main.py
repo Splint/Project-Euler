98c1d50ad84619b61cbad8d679d0f9f63ad9ee96
Sang Shin <shinsa@onid.orst.edu>
2011-12-20 02:06:09 -0800

    Finished main.py for P4

--- P4/main.py
+++ P4/main.py
@@ -13,12 +13,8 @@ def isPalindrome(n):
 def main():
     largest_pal_num = 0
     answer = 0
+
     print "Project Euler Problem #4"
-    print "A palindromic number reads the same both ways. The largest palindrome made from"
-    print "the product of two 2-digit numbers is 9009 = 91 x 99."
-    print
-    print "Find the largest palindrome made from the product of two 3 digit numbers."
-    print
     for first_num in range(MAX_NUM, MIN_NUM, -1):
         for second_num in range(MAX_NUM, MIN_NUM, -1):
             answer = first_num * second_num
