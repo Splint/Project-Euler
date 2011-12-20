a27ad4756c2a30f5be4d62e115747d2d28beaf25
Sang Shin <shinsa@onid.orst.edu>
2011-12-20 02:08:39 -0800

    Finished main.py for P9

--- P9/main.py
+++ P9/main.py
@@ -13,13 +13,8 @@ def isWhole(n):
 def main():
     result = 0
     done = False
+
     print "Project Euler Problem #9"
-    print "A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,\n"
-    print "a^2 + b^2 = c^2\n"
-    print "For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2\n"
-    print "There exists exactly one Pythagorean triplet for which a + b + c = 1000."
-    print "Find the product abc.\n"
-    
     for a in range(1, 10000):
         if done:
             '''We need to break out of the nested for loop somehow'''
