cf37e331355284855e51c499371a8f22510fbdb5
Sang Shin <shinsa@onid.orst.edu>
2011-12-20 02:05:26 -0800

    Finished main.py for P3

--- P3/main.py
+++ P3/main.py
@@ -11,10 +11,8 @@ def isPrime(n):
 
 def main():
     largest_prime_num = 0
+   
     print "Project Euler Problem #3"
-    print "The prime factors of 13195 are 5, 7, 13, and 29."
-    print "What is the largest prime factor of the number 600851475143?"
-    print
     for num in range(1, int(TARGET_NUM**0.5)+1):
         if TARGET_NUM % num == 0:
             if (isPrime(num) == True):
